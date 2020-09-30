3cdc41f96b9f3cd31bb6e22608dc601753304c40
Niels Grewe <grewe@ocean-insights.com>
2020-09-30 21:38:32 +0200

    fix: Don't include react in the main bundle
    cf. faucet-pipeline/faucet-pipeline-js#144

--- faucet.config.js
+++ faucet.config.js
@@ -1,3 +1,5 @@
+const kind = process.env.NODE_ENV == "production" ? "production.min" : "development";
+
 module.exports = {
 	watchDirs: [
 		"./app"
@@ -6,8 +8,22 @@ module.exports = {
 		source: "./app/index.jsx",
 		target: "./dist/index.js",
 		jsx: true,
-		esnext: {exclude: ["*"]}
+		esnext: {exclude: ["*"]},
+		externals: {
+			react: "React",
+			"react-dom": "ReactDOM",
+		}
 	}],
+	static: [
+        {
+            source: `react/umd/react.${kind}.js`,
+            target: "./dist/react.js",
+        },
+        {
+            source: `react-dom/umd/react-dom.${kind}.js`,
+            target: "./dist/react-dom.js",
+        }
+    ],
 	manifest: {
         target: "./manifest.json",
         key: "short",
